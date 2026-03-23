ae5f88cd580349f1f91dd17161e3bf510dacc39a
Franz Gabriel A. Tuzon <franzgabriel.tuzon@neu.edu.ph>
2026-03-23 02:28:06 +0000

    Runtime ReferenceError
    cn is not defined
    
    src/app/visitor/check-in/pag

--- src/app/visitor/check-in/page.tsx
+++ src/app/visitor/check-in/page.tsx
@@ -8,6 +8,7 @@ import { addLog } from '@/lib/mock-db';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent } from '@/components/ui/card';
 import { useToast } from '@/hooks/use-toast';
+import { cn } from '@/lib/utils';
 import { 
   Book, 
   Search, 
